5a960319e4ec3ea6640e3a9ff0898c4f3df1ffcf
sk8erchoi <sk8er.choi@gmail.com>
2022-04-10 04:04:24 +0900

    feat: select itag

--- youtube_video_download.py
+++ youtube_video_download.py
@@ -1,6 +1,6 @@
 import sys
-from pathlib import Path
 import os
+from os.path import splitext
 
 from pytube import YouTube
 from pytube.cli import on_progress
@@ -15,25 +15,30 @@ def download(video_id):
     url = "https://www.youtube.com/watch?v=" + video_id
     yt = YouTube(url, on_progress_callback=on_progress)
     
-    vfile = yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first().download(save_dir)
-    title = Path(vfile).stem
-    print("title", title)
-    os.rename(vfile, title + "(video only).mp4")
+    for stream in yt.streams.filter(only_video=True):
+        print(stream)
+
+    itag = input("select itag:")
+    video = yt.streams.get_by_itag(itag).download()
+    video_only = splitext(video)[0] + "(video only)" + splitext(video)[1]
+    os.rename(video, video_only)
     
-    afile = yt.streams.filter(only_audio=True).order_by("abr").desc().first().download(save_dir)
-    os.rename(afile, title + "(audio only).mp4")
-    return title
+    audio = yt.streams.filter(only_audio=True).order_by("abr").desc().first().download(save_dir)
+    audio_only = splitext(audio)[0] + "(audio only)" + splitext(audio)[1]
+    os.rename(audio, audio_only)
+    return audio_only, video_only, video 
 
 
-def mix(title):
-    video_stream = ffmpeg.input(title + "(video only).mp4")
-    audio_stream = ffmpeg.input(title + "(audio only).mp4")
-    ffmpeg.output(audio_stream, video_stream, title + '.mp4').run()
+def mix(audio, video, av):
+    audio_stream = ffmpeg.input(audio)
+    video_stream = ffmpeg.input(video)
+    
+    ffmpeg.output(audio_stream, video_stream, av).run()
 
 
 def main(video_id):
-    title = download(video_id)
-    mix(title)
+    audio, video, av = download(video_id)
+    mix(audio, video, av)
 
 
 if __name__ == '__main__':
